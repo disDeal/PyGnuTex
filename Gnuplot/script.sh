0aeda2a6de196469ab4fb28af504269c17307e6d
Alex Komissarov <k.a.komissar@gmail.com>
2019-11-08 15:41:44 +0500

    Move out from folder

--- Gnuplot/script.sh
+++
@@ -1,8 +0,0 @@
-gnuplot GnuScript.gp
-latex build.tex; dvipdf build.dvi
-pdfcrop build.pdf crap2.pdf
-pdf2ps crap2.pdf crap2.ps
-ps2eps -f crap2.ps
-eps2svg crap2.eps crap2EPS.svg
-pdf2svg crap2.pdf crap2PDF.svg
-rm build.dvi build.aux crap2.ps build.pdf
--- Gnuplot/test.py
+++
@@ -1,4 +0,0 @@
-a = ""
-aList = ["1", "2", "3"]
-print(aList, aList[0], "len=", len(a), type(a))
-print(str(type(a)))
