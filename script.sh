21a942b98082fb3c20857024ba3764c2024d8d13
Alex Komissarov <k.a.komissar@gmail.com>
2020-03-06 15:40:41 +0500

    Cleaning

--- script.sh
+++
@@ -1,8 +0,0 @@
-gnuplot GnuScript.gp
-latex build.tex; dvipdf build.dvi
-pdfcrop build.pdf crap.pdf
-pdf2ps crap.pdf crap.ps
-ps2eps -f crap.ps
-eps2svg crap.eps crapEPS.svg
-pdf2svg crap.pdf crapPDF.svg
-rm build.dvi build.aux crap.ps build.pdf
